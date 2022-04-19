d22b3867a85814082a36dad29f9aebe9a88b2f7b
Barzin <barzin.development@gmail.com>
2022-04-19 10:12:01 +0200

    Add Teachers solution to Exercise1_html_css_refresher. Alos downloaded all the solutions of Frontend path modules for later reference.

---
+++ Part1_Front_end_Development/solutions-exercises-master/gatsby/days_of_the_week/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <p>This site features the days of the week.</p>
+    </Layout>
+  );
+}
