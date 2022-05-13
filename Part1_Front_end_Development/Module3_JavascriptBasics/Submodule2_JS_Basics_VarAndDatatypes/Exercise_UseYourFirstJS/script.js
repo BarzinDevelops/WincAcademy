73a2f428557c37857d128569dc53a40185d58402
Barzin <barzin.development@gmail.com>
2022-05-13 18:45:51 +0200

    Exercise1 of Module3 JS.

---
+++ Part1_Front_end_Development/Module3_JavascriptBasics/Submodule2_JS_Basics_VarAndDatatypes/Exercise_UseYourFirstJS/script.js
@@ -0,0 +1,42 @@
+
+// 5. Use let to create (initialize) a variable and store your own name in the variable. 
+//    Then console.log() your own name.
+console.log("Results assignment 5:");
+let myName = 'Barzin';
+console.log(myName);
+
+// 7. Add the following variables together and log the result:
+console.log("Results assignment 7:");
+let numbers = 4+4;
+console.log(numbers);
+
+let strings = "4" + "4";
+console.log(strings);
+
+// 8. Make some simple sums with the following operators and log the result to the console. Use:
+console.log("Results assignment 8:");
+// Addition:
+console.log(4+6);
+console.log(6-2);
+console.log(4*6);
+console.log(6/4);
+console.log(6%4); //Modulus (division remainder)
+
+// 9. Create a variable with your age in numbers (for example 48), 
+//    log the type of the variable to the console with typeof (operator type)
+console.log("Results assignment 9:");
+let myAge = 39;
+console.log(typeof myAge);
+
+
+
+// 10. Now change the type of this variable by writing out your age ("48") in "text", 
+//     log the type to the console with typeof.
+console.log("Results assignment 10:");
+
+myAge = "39";
+console.log(typeof myAge);
+
+
+
+
