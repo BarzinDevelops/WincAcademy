83ca216546b5a46f1f44fe0c6df1295679f2e96a
Barzin <barzin.development@gmail.com>
2022-05-14 02:07:07 +0200

    Add Exercise2 and 3. These are about comments, if/else statements which run on node.

---
+++ Part1_Front_end_Development/Module3_JavascriptBasics/Submodule2_JS_Basics_VarAndDatatypes/Exercise2_RunJsOnPcAndUseComments/script.js
@@ -0,0 +1,8 @@
+// Part 1:
+// console.log("Hello Winc Academy students.");
+
+// Part 2:
+//This is a large sum.
+/* console.log(`1230941 * 1823794 = ${1230941 * 1823794}`);
+console.log(`637263  / 54 = ${637263 / 54}`); */
+// A piece of code that is in a comment is no longer read by the javascript engine.
---
+++ Part1_Front_end_Development/Module3_JavascriptBasics/Submodule2_JS_Basics_VarAndDatatypes/Exercise3_UseIfElseStatements/script.js
@@ -0,0 +1,28 @@
+// Part1:
+const age = 18;
+
+if (age >= 18) {
+  console.log("Your allowed to enter my club.");
+} else {
+  console.log("You're too young my friend, please stay outside.");
+}
+
+// Part2:
+const isFemale = false;
+
+if (isFemale) {
+  console.log("Good evening, welcome to ladies night.");
+} else {
+  console.log("Sorry sir, tonight is for ladies only.");
+}
+
+// Part 3:
+const driverStatus = "not-designated";
+
+if (driverStatus === "designated") {
+  console.log("You've designated status, drive save.");
+} else {
+  console.log(
+    "Sorry you don't have a designated status, therefore you are not allowed to drive!!"
+  );
+}
