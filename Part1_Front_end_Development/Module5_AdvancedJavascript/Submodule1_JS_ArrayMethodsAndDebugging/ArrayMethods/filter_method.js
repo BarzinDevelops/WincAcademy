fb0a21dad97440a4d2b3b7de76ab96d7f0a3c16d
Barzin <barzin.development@gmail.com>
2022-06-23 14:55:08 +0200

    Add some usefull commands for git use. Also setup of new module5 (javacsript Advanced topics). Started with first submodule and play around with filter method as one of the array-methods in js.

---
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Submodule1_JS_ArrayMethodsAndDebugging/ArrayMethods/filter_method.js
@@ -0,0 +1,32 @@
+/* Definition and Usage
+The filter() method creates a new array filled with elements that pass a test provided by a function.
+The filter() method does not execute the function for empty elements.
+The filter() method does not change the original array. 
+
+Syntax:
+-------
+array.filter(function(currentValue, index, arr), thisValue)
+
+Parameters:
+----------
+Parameter	    Description
+--------        -----------
+function()	    Required. A function to run for each array element.
+currentValue	Required. The value of the current element.
+index	        Optional. The index of the current element.
+arr	            Optional. The array of the current element.
+thisValue	    Optional. Default undefined. A value passed to the function as its this value.
+
+Return Value:
+-------------
+An array	Containing the elements that pass the test.
+            If no elements pass the test it returns an empty array.
+*/
+
+const ages = [19, 18, 16, 12, 17, 8, 39, 37, 33, 57, 28];
+const result = ages.filter((age) =>{
+    return age >= 18;
+})
+
+console.log(`typeof(result) -> ${typeof(result)}`);
+console.log(result);
