6acd403aa5378955013c8292a602e33e7aa07c31
Barzin <barzin.development@gmail.com>
2022-06-29 14:20:12 +0200

    Rename files to the right method name. Also practise code for every() method.

---
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Submodule1_JS_ArrayMethodsAndDebugging/ArrayMethods/ArrayMethod5_every/every_method.js
@@ -0,0 +1,35 @@
+/* Definition and Usage
+The every() method executes a function for each array element.
+The every() method returns true if the function returns true for all elements.
+The every() method returns false if the function returns false for one element.
+The every() method does not execute the function for empty elements.
+
+The every() method does not change the original array
+
+Syntax:
+-------
+array.every(function(currentValue, index, arr), thisValue)
+
+Parameters:
+----------
+Parameter	    Description
+--------        -----------
+function()	    Required. A function to run for each array element.
+currentValue	  Required. The value of the current element.
+index	          Optional. The index of the current element.
+arr	            Optional. The array of the current element.
+thisValue       Optional. Default undefined. A value passed to the function as its this value.
+
+Return Value:
+-------------
+A boolean	      true if all elements pass the test, otherwise false.
+
+*/
+
+const ages = [32, 33, 19, 40];
+
+console.log(`Is there anyone above 18 years old? => ${ages.every(checkAdult)}`);
+
+function checkAdult(age) {
+  return age > 18;
+}
