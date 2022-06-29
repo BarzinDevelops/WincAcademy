23b554f6263a3968611642bccf2ddee5b4545daf
Barzin <barzin.development@gmail.com>
2022-06-29 22:28:49 +0200

    Ranamed Exercise2 array methods part2 -> Exercise3 arraymethod part 2. Also added part of the assignment as start point for this Exercise.

---
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Submodule1_JS_ArrayMethodsAndDebugging/Exercise3_ArrayMehtods_part2/app.js
@@ -0,0 +1,43 @@
+/* Exercise: Array methods - part 2
+
+For each of the exercises below, you need one of the array methods below. 
+To solve the puzzle, you first need the test data. 
+There are several possible solutions for 1 problem.
+
+[ ] .find method
+[ ] .forEach method
+[ ] .some method
+[ ] .every method
+[ ] .includes method
+------------------------------------------
+The next 3 methods are a bit advanced. 
+There are several exercises where you can use these functions. 
+We're going to practice with it more later today:
+
+[ ] .map method
+[ ] .filter method
+[ ] .reduce method
+____________________________________________________________________________
+*/
+
+/*
+A Write a JavaScript function that finds a certain item and returns 
+it based on a certain value. 
+Find the entire Spiderman object, based on the name "Spiderman". Use .find. 
+ */
+console.log(`--------------------
+Part A:
+------------------`)
+
+
+// Test dates:
+const superheroes = [
+    {name: "Batman", alter_ego: "Bruce Wayne"},
+    {name: "Superman", alter_ego: "Clark Kent"},
+    {name: "Spiderman", alter_ego: "Peter Parker"}]
+    
+    // Here goes your function
+    
+    console.log(findSpiderMan(superheroes))
+    // Find Spiderman
+    // result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
