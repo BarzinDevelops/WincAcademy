f564d85c2e5939cb54b70024a2e8de17ee7adc34
Barzin <barzin.development@gmail.com>
2022-07-12 18:43:19 +0200

    Add radio btns for filters, the posters by using data from moviesDB.js (contains all movies and their info) and created addMoviesToDom() so that each poster becomes an new li dynamically.

--- Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
@@ -3,15 +3,29 @@ const navContainer = document.getElementById('nav-container');
 const movieFilters = navContainer.querySelectorAll('[name="mv-filter"]');
 
 const mainContainer = document.querySelector('.main-container');
-const moviesList = document.getElementById('mv-list');
+
 
 
 movieFilters.forEach(item => {
-    console.log(item);
+    // console.log(item);
     item.addEventListener('input', ev =>{
         // console.log("this event has happend: ", ev)
-        console.log("this event has target.value.defaultValue: ", ev.target.defaultValue)
+        // console.log("this event has target.value.defaultValue: ", ev.target.defaultValue)
         // console.log("this is event of item: ", ev.currentTarget)
     })
 });
 
+const addMoviesToDom = (moviesData) => {
+    const moviesListUl = document.getElementById('mv-list');
+    moviesData
+    .map(movie => movie)
+    .forEach(movie =>  {
+        let newItem = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = movie.poster
+        newItem.appendChild(img)
+        moviesListUl.appendChild(newItem);
+    })
+
+}
+addMoviesToDom(movies);
