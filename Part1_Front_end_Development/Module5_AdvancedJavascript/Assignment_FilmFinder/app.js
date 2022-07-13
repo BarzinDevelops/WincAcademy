ed50c1d3d7bea666d98de69955da11c853931c6f
Barzin <barzin.development@gmail.com>
2022-07-14 00:04:56 +0200

    Test each filter to see if the result contains all posters of movies, selected based on chosen filter and no other. For Latest movie change value of filter to be 2014 and use this to compare the movies year attribute value >= 2014. Use this condition in the current filterMovies() by adding a or operator and coverting the values to Numbers.

--- Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
@@ -3,17 +3,17 @@ const navContainer = document.getElementById('nav-container');
 const mvRadioBtnFilters = navContainer.querySelectorAll('[name="mv-filter"]');
 
 const mainContainer = document.querySelector('.main-container');
-
+const moviesListUl = document.getElementById('mv-list');
 
 mvRadioBtnFilters.forEach(item => {
-    item.addEventListener('change', ev =>{
-        // console.log("radiobtn event has triggered")
+    item.addEventListener('input', ev =>{
         handleOnChangeEvent(ev);
     })
 });
 
 const addMoviesToDom = (moviesData) => {
-    const moviesListUl = document.getElementById('mv-list');
+    moviesListUl.innerHTML = ""
+    
     moviesData
     .map(movie => movie)
     .forEach(movie =>  {
@@ -34,38 +34,29 @@ const handleOnChangeEvent = event => {
     // console.log("handleOnChangeEvent() event.target", event.target.id)
 
     switch (event.target.id) {
-        case 'latest-mv':
-            console.log(filterMovies(event.target.value))
+        case 'latest-mv':    
             addMoviesToDom(filterMovies(event.target.value))
             break;
         case 'av-mv':
-            console.log(filterMovies(event.target.value))
+            addMoviesToDom(filterMovies(event.target.value))
             break;
         case 'xm-mv':
-            console.log(filterMovies(event.target.value))
+            addMoviesToDom(filterMovies(event.target.value))
             break;
         case 'pr-mv':
-            console.log(filterMovies(event.target.value))
+            addMoviesToDom(filterMovies(event.target.value))
             break;
         case 'bat-mv':
-            console.log(filterMovies(event.target.value))
+            addMoviesToDom(filterMovies(event.target.value))
             break;
         default:
             console.log("the filter in switch() doesnt seem to work!!")
             break;
     }
 }
-/* 
-Write the filterMovies function. 
-Check if the word appears in 
-the title of the movie. You will now only have a list of 
-the filtered movies over. As the last step, 
-you want to call this filterMovies, the function from
- step 4 addMoviestoDom(), but now with your filtered movies list, 
- instead of all the movies list.
-*/
 
 const filterMovies = wordInMovie =>{
+    console.log(movies)
    return movies
     .filter(movie => movie.title.toLowerCase().includes(wordInMovie) || 
     Number(movie.year) >= Number(wordInMovie));
