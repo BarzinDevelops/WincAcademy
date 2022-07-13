0d3b52eb7f241745b8883856017e1d1f06172f18
Barzin <barzin.development@gmail.com>
2022-07-13 22:50:22 +0200

    Add function using swtich to determine wich filter is activated (which radio btn). Add function to filter movies based on chosen filter categorie and return movies to addMoviesToDom() so the right poster gets rendered in html page.

--- Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
+++ Part1_Front_end_Development/Module5_AdvancedJavascript/Assignment_FilmFinder/app.js
@@ -1,17 +1,14 @@
 
 const navContainer = document.getElementById('nav-container');
-const movieFilters = navContainer.querySelectorAll('[name="mv-filter"]');
+const mvRadioBtnFilters = navContainer.querySelectorAll('[name="mv-filter"]');
 
 const mainContainer = document.querySelector('.main-container');
 
 
-
-movieFilters.forEach(item => {
-    // console.log(item);
-    item.addEventListener('input', ev =>{
-        // console.log("this event has happend: ", ev)
-        // console.log("this event has target.value.defaultValue: ", ev.target.defaultValue)
-        // console.log("this is event of item: ", ev.currentTarget)
+mvRadioBtnFilters.forEach(item => {
+    item.addEventListener('change', ev =>{
+        // console.log("radiobtn event has triggered")
+        handleOnChangeEvent(ev);
     })
 });
 
@@ -21,6 +18,7 @@ const addMoviesToDom = (moviesData) => {
     .map(movie => movie)
     .forEach(movie =>  {
         let newItem = document.createElement('li');
+        newItem.id = 'mv-list-item'
         const img = document.createElement('img');
         img.src = movie.poster
         newItem.appendChild(img)
@@ -28,4 +26,47 @@ const addMoviesToDom = (moviesData) => {
     })
 
 }
-addMoviesToDom(movies);
+// addMoviesToDom(movies);
+
+
+const handleOnChangeEvent = event => {
+    // console.log("handleOnChangeEvent() event.target", event.target)
+    // console.log("handleOnChangeEvent() event.target", event.target.id)
+
+    switch (event.target.id) {
+        case 'latest-mv':
+            console.log(filterMovies(event.target.value))
+            addMoviesToDom(filterMovies(event.target.value))
+            break;
+        case 'av-mv':
+            console.log(filterMovies(event.target.value))
+            break;
+        case 'xm-mv':
+            console.log(filterMovies(event.target.value))
+            break;
+        case 'pr-mv':
+            console.log(filterMovies(event.target.value))
+            break;
+        case 'bat-mv':
+            console.log(filterMovies(event.target.value))
+            break;
+        default:
+            console.log("the filter in switch() doesnt seem to work!!")
+            break;
+    }
+}
+/* 
+Write the filterMovies function. 
+Check if the word appears in 
+the title of the movie. You will now only have a list of 
+the filtered movies over. As the last step, 
+you want to call this filterMovies, the function from
+ step 4 addMoviestoDom(), but now with your filtered movies list, 
+ instead of all the movies list.
+*/
+
+const filterMovies = wordInMovie =>{
+   return movies
+    .filter(movie => movie.title.toLowerCase().includes(wordInMovie) || 
+    Number(movie.year) >= Number(wordInMovie));
+}
