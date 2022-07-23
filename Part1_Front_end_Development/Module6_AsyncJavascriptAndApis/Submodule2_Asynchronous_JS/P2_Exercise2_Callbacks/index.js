9418895081427dfae15eb5a7e1f9699e84c84948
Barzin <barzin.development@gmail.com>
2022-07-23 22:03:33 +0200

    Refactor exercise2 (about using callbacks).

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P2_Exercise2_Callbacks/index.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P2_Exercise2_Callbacks/index.js
@@ -1,10 +1,12 @@
-/*
-Step 1
 
-Create a function and name it makeHomework , this function takes two arguments. You can name the first argument course and the second argument callback.
-The makeHomework function outputs a console.log that reads: "Okay, ok, I'm going to do my [insert course here] homework now".
 
-*/
+// Step 1
+// Create a function and name it makeHomework , this function 
+// takes two arguments. You can name the first argument course 
+// and the second argument callback. The makeHomework function 
+// outputs a console.log that reads: 
+// "Okay, ok, I'm going to do my [insert course here] homework now".
+
 const log = console.log;
 
 function makeHomework(course, callback) {
@@ -26,24 +28,25 @@ function makeHomework(course, callback) {
 
 }
 
-/*
-Step 2
-Under the now complete function of makeHomework, create a separate function that you call doneWithHomework. It does not accept arguments.
-The doneWithHomework function runs a console.log that says "Look, Mom/Dad, I'm done with my homework!".
-*/
+
+// Step 2
+// Under the now complete function of makeHomework, create a separate 
+// function that you call doneWithHomework. It does not accept arguments.
+// The doneWithHomework function runs a console.log that says "Look, 
+// Mom/Dad, I'm done with my homework!".
+
 function doneWithHomework() {
   log(`Look, Mom/Dad, I'm done with my homework!`);
 }
 
-/*
-Step 3
 
-Under both functions you can now call the Make homework function with two arguments. The first takes the string "math", the second argument is the function doneWithHomework.
-*/
+// Step 3
+// Under both functions you can now call the Make homework function with 
+// two arguments. The first takes the string "math", the second argument 
+// is the function doneWithHomework.
+
 makeHomework("math", doneWithHomework);
 
-/*
-Step 4
 
-Use SetTimeOut() in your makeHomework function.
-*/
+// Step 4
+// Use SetTimeOut() in your makeHomework function.
