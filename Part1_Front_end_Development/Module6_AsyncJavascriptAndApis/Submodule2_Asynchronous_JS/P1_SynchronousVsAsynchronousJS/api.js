9e24930d25adfa12d1d0e95f5a0e94b9ec53979f
Barzin <barzin.development@gmail.com>
2022-07-23 15:18:08 +0200

    Add exercise2 (about using callbacks).

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P1_SynchronousVsAsynchronousJS/api.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P1_SynchronousVsAsynchronousJS/api.js
@@ -18,3 +18,17 @@ const myWindow = window.open("", "", "width=200, height=100");
 myWindow.document.write("<body style='background: red;'>this is test</body>");
 
 setTimeout(function() {myWindow.close()}, 4000);
+
+
+/* 
+Learning goals:
+
+I can explain in what way, JavaScript is Synchronous.
+    it can only execute one command at a time 
+    and the other commands need to wait for executing 
+    before the running command executes.
+
+I can explain why you may sometimes need Asynchronous JavaScript.
+    
+I can use a setTimeout() myself, to test async JavaScript.
+*/
---
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P2_Exercise2_Callbacks/index.js
@@ -0,0 +1,49 @@
+/*
+Step 1
+
+Create a function and name it makeHomework , this function takes two arguments. You can name the first argument course and the second argument callback.
+The makeHomework function outputs a console.log that reads: "Okay, ok, I'm going to do my [insert course here] homework now".
+
+*/
+const log = console.log;
+
+function makeHomework(course, callback) {
+  log(`Okay, ok, I'm going to do my ${course} homework now.`);
+
+// Just to count passed miliseconds
+    let trigger=1; 
+    myInterval = setInterval(() => {
+        console.log(trigger, "second passed..");
+        if(trigger === 3){
+            clearInterval(myInterval)
+        }
+        trigger++;
+    }, 1000);
+
+  setTimeout(() => {
+    callback();
+  }, 3000);
+
+}
+
+/*
+Step 2
+Under the now complete function of makeHomework, create a separate function that you call doneWithHomework. It does not accept arguments.
+The doneWithHomework function runs a console.log that says "Look, Mom/Dad, I'm done with my homework!".
+*/
+function doneWithHomework() {
+  log(`Look, Mom/Dad, I'm done with my homework!`);
+}
+
+/*
+Step 3
+
+Under both functions you can now call the Make homework function with two arguments. The first takes the string "math", the second argument is the function doneWithHomework.
+*/
+makeHomework("math", doneWithHomework);
+
+/*
+Step 4
+
+Use SetTimeOut() in your makeHomework function.
+*/
