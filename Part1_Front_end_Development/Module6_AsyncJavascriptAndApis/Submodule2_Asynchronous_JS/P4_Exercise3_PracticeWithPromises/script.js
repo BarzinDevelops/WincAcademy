6689c1ac375b64c806f2141f468fdfaad9281056
Barzin <barzin.development@gmail.com>
2022-07-25 13:22:39 +0200

    Add the Second part of this Exercise3 about promises.

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P4_Exercise3_PracticeWithPromises/script.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P4_Exercise3_PracticeWithPromises/script.js
@@ -7,7 +7,6 @@ Write a function testNum that takes a number as an argument and returns a
 Promise that tests if the value is less than or greater than the value 10. 
 Log the result to the console.
 */
-
 /* const testNum = (number) => { 
     return new Promise((resolve, reject)=>{
         if(number < 10) resolve(`${number} is less than value 10`);
@@ -25,3 +24,52 @@ testNum(10);*/
  
 
 
+/*
+Exercise 2:
+Write two functions that use Promises that you can chain! 
+The first function, makeAllCaps(),
+    will take in an array of words and capitalize them, 
+The second function, sortWords(), 
+    will sort the words in alphabetical order. 
+If the array contains anything but strings, it should throw an error.
+Then call these functions by *chaining* the promises
+*/
+
+const makeAllCaps = (wordsArr) => {
+    return new Promise( (resolve, reject) => {
+        // log("wordsArr", wordsArr)
+       const checkIfString = wordsArr.every(i => (typeof i === 'string'));
+       if(checkIfString) resolve( wordsArr.map(word => word.toUpperCase()) )
+       else reject(`Error: Your array must only contain strings!!`);
+    } );
+};  
+
+
+
+
+const sortWords = (capArr) => {
+    // log("capArr", capArr)
+    return new Promise((resolve, reject) => resolve(capArr.sort()));
+};
+
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+const otherArrayOfWords = ['promise', 'break', 'practise'];
+const complicatedArray = ['cucumber', 44, true];
+
+// call both functions, chain them together and log the result to the console
+makeAllCaps(arrayOfWords)
+    .then(capWords => sortWords(capWords))
+    .then(sortedCapWords => log("The sorted Capitalized Words of your array are: ",sortedCapWords))
+    .catch(err => log(err.message ? err.message : err));
+
+// trying with other words array:    
+makeAllCaps(otherArrayOfWords)
+    .then(capWords => sortWords(capWords))
+    .then(sortedCapWords => log("The sorted Capitalized Words of your array are: ",sortedCapWords))
+    .catch(err => log(err.message ? err.message : err));
+
+// checking functionality with a array that causes error!
+makeAllCaps(complicatedArray)
+    .then(capWords => sortWords(capWords))
+    .then(sortedCapWords => log(sortedCapWords))
+    .catch(err => log(err.message ? err.message : err));
