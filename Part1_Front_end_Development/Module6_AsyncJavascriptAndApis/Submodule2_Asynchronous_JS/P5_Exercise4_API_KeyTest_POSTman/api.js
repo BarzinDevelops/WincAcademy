41067de67974c7a2ea89f500cb13ba6ba29320ed
Barzin <barzin.development@gmail.com>
2022-07-27 15:51:29 +0200

    Refactor code for Exercise4: API Keyy test with POSTman, based on feedback from teacher. The endpoint shouldn't be adjusted, only an extra key/value needed to be added to the parameters in POSTman get request for Part3.

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P5_Exercise4_API_KeyTest_POSTman/api.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule2_Asynchronous_JS/P5_Exercise4_API_KeyTest_POSTman/api.js
@@ -77,15 +77,51 @@ Now add the following to the parameters.
 When you add them to the parameters you will see them 
 appear in your request URL.
 
-Do the GET request again for endpoint: http://www.omdbapi.com/?apikey=896f18d6
+Do the GET request again for endpoint: http://www.omdbapi.com/?i=tt3896198
 key: apikey value: 896f18d6
     
     Status code: 200OK
     What response do you get now?
-    {
-        "Response": "False",
-        "Error": "Incorrect IMDb ID."
-    }
+        {
+            "Title": "Guardians of the Galaxy Vol. 2",
+            "Year": "2017",
+            "Rated": "PG-13",
+            "Released": "05 May 2017",
+            "Runtime": "136 min",
+            "Genre": "Action, Adventure, Comedy",
+            "Director": "James Gunn",
+            "Writer": "James Gunn, Dan Abnett, Andy Lanning",
+            "Actors": "Chris Pratt, Zoe Saldana, Dave Bautista",
+            "Plot": "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
+            "Language": "English",
+            "Country": "United States",
+            "Awards": "Nominated for 1 Oscar. 15 wins & 59 nominations total",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+            "Ratings": [
+                {
+                    "Source": "Internet Movie Database",
+                    "Value": "7.6/10"
+                },
+                {
+                    "Source": "Rotten Tomatoes",
+                    "Value": "85%"
+                },
+                {
+                    "Source": "Metacritic",
+                    "Value": "67/100"
+                }
+            ],
+            "Metascore": "67",
+            "imdbRating": "7.6",
+            "imdbVotes": "660,387",
+            "imdbID": "tt3896198",
+            "Type": "movie",
+            "DVD": "22 Aug 2017",
+            "BoxOffice": "$389,813,101",
+            "Production": "N/A",
+            "Website": "N/A",
+            "Response": "True"
+        }
 --------------------------------------
 Can you recognize some data from the movie finder?
 */
