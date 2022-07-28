868927d03ec40d6845573e8d690cb7b0a7c40408
Barzin <barzin.development@gmail.com>
2022-07-28 19:07:36 +0200

    Add Exercise1 about spread and rest operator usage.

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule3_JS_AsyncAndAwait/P1_Exercise1_RestAndSpread_Operator/app.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule3_JS_AsyncAndAwait/P1_Exercise1_RestAndSpread_Operator/app.js
@@ -1,24 +1,34 @@
 const log = console.log;
 
-/*Exercise: Rest Parameter and Spread Operator
-You are going to write 2 functions.
+// Exercise: Rest Parameter and Spread Operator
+// You are going to write 2 functions.
 
-Function 1:
+/*Function 1: Write a simple function that adds multiple numbers together
+            The function accepts multiple arguments
+            Use the rest parameter to make sure your function treats arguments as an array.
 
-Write a simple function that adds multiple numbers together
-The function accepts multiple arguments
-Use the rest parameter to make sure your function treats arguments as an array.
-*/
+// Solution to Function1:
 const addNum = (...numbers) =>{
     return numbers.reduce((prev, curr)=> prev + curr);
 }
 
-log(addNum(5,7,3,15,2,18,33));
-/*
-Function 2:
+log(addNum(5,7,3,15,2,18,33)); */
 
-Write a simple function that adds multiple numbers together
-The function takes 1 argument, which is an array of numbers
-Use the spread operator to make sure your function sums up all items in the array. 
+
+
+
+
+
+
+/*Function 2: Write a simple function that adds multiple numbers together
+            The function takes 1 argument, which is an array of numbers
+            Use the spread operator to make sure your function sums up all items in the array. 
 
 */
+
+//Implementatie met gebruik van Rest operator als function parameter:  
+const addNumUsingSpread = (...nums) => nums.reduce((prev, curr)=> prev + curr);
+  
+  const numbers = [1, 2, 3, 13, 11];
+  console.log(addNumUsingSpread(...numbers));
+  console.log(addNumUsingSpread(22,33,12,8)); 
