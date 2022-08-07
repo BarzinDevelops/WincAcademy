991cb4d9870bbebc159074509792af351f033b52
Barzin <barzin.development@gmail.com>
2022-08-07 20:16:12 +0200

    Add sort() to the current country list so it's sorted by country name.

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule4_BigChuncksOfData/Exercise1_WorkingWithLargeArraysAndObjects/script.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule4_BigChuncksOfData/Exercise1_WorkingWithLargeArraysAndObjects/script.js
@@ -8,12 +8,14 @@ const capricornWomenBtn = document.getElementById('capricornwomen');
 const listTitle = document.getElementById('')
 
 
+/* Sub-exercise: country list - 1 point
+    List all countries, sorted by country name. */
 
 // Get county of eacht person in randomPersonData array:
 const getCountries = randomPersonData.map(person => person.region);
 
 // return an array of countries with no doubles (using Set()): 
-const countryList = [...new Set(getCountries)];
+const countryList = [...new Set(getCountries)].sort();
 
 const render = arrayToRender => {
     arrayToRender.forEach(element => {
@@ -22,20 +24,30 @@ const render = arrayToRender => {
         resultsContainer.appendChild(newItem);
     });
 }
-
-// SubExercise1_CountryList
 countryListBtn.addEventListener('click', ()=>{
     resultsContainer.replaceChildren();
     titleOfLists.textContent = countryListBtn.innerText;
     resultsContainer.append(titleOfLists);
     render(countryList);
 });
+// -------------------------------------------------------------//
+/* Sub exercise: Capricorn women - 3 points
+Make a list of people:
+show their first and last name and photo
+sort the list by their first name
+every person on that list must
+be a woman
+be older than 30 (1990 or older)
+be a Capricorn (birthday from December 22 to January 19) */
 
 
-// SubExercise2_CapricornWomen  
 capricornWomenBtn.addEventListener('click', ()=>{
     resultsContainer.replaceChildren();
     titleOfLists.textContent = capricornWomenBtn.innerText;
     resultsContainer.append(titleOfLists);
     // render(countryList);
-})
+});
+
+
+
+// ----------------------------------------------------------//
