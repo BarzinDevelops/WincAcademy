a642aee64948b7d6262fe5c90876aff61fa25926
Barzin <barzin.development@gmail.com>
2022-08-09 16:16:39 +0200

    Refactored my code based on solution (winc). Most challenge was sortByName() and sort_helper() that Winc solution uses, to get sorting of the capricorn names done.

--- Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule4_BigChuncksOfData/Exercise1_WorkingWithLargeArraysAndObjects/SubExercise2_CapricornWomen/capricorn.js
+++ Part1_Front_end_Development/Module6_AsyncJavascriptAndApis/Submodule4_BigChuncksOfData/Exercise1_WorkingWithLargeArraysAndObjects/SubExercise2_CapricornWomen/capricorn.js
@@ -29,32 +29,39 @@ if(person.gender === "female" & person.age > 30){
 }
 });
 
-const renderCapriWoman = (...myRest) => {
-myRest.forEach(element => {
+
+// This function gets called from renderCapriWoman() to sort the names in capriWoman
+// before it gets rendered to page. 
+const sortNames = (pers1, pers2) => pers1.name > pers2.name ? 1 : -1;
+
+const renderCapriWoman = (...receivedCapriWomen) => {
+
+    receivedCapriWomen.sort(sortNames);
+    receivedCapriWomen.forEach(women => {
 
     const persContainer = document.createElement('div');
     persContainer.classList.add('capri-women');
     resultsContainer.appendChild(persContainer);
 
     const newItem_name = document.createElement('li');
-    newItem_name.textContent = `Name: ${element.name}`;
+    newItem_name.textContent = `Name: ${women.name}`;
     newItem_name.style.color = 'purple';
 
     const newItem_surname = document.createElement('li');
-    newItem_surname.textContent = `Surname: ${element.surname}`;
+    newItem_surname.textContent = `Surname: ${women.surname}`;
     newItem_surname.style.color = 'blue';
 
     const newItem_age = document.createElement('li');
-    newItem_age.textContent = `Age: ${element.age}`;
+    newItem_age.textContent = `Age: ${women.age}`;
     newItem_age.style.color = 'purple';
 
     const newItem_birthday = document.createElement('li');
-    newItem_birthday.textContent = `Birthday: ${element.birthday}`;
+    newItem_birthday.textContent = `Birthday: ${women.birthday}`;
     newItem_birthday.style.color = 'blue';
 
     const newItem_photo = document.createElement('img'); 
     newItem_photo.classList.add('person-photo');
-    newItem_photo.src = element.photo;
+    newItem_photo.src = women.photo;
 
     persContainer.appendChild(newItem_name);
     persContainer.appendChild(newItem_surname);
