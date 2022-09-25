2ed04839a7dcd34eb59414fb8303b76cb75b3ce7
Barzin <barzin.development@gmail.com>
2022-09-25 10:56:44 +0200

    Run differen tests in Exercise1 using jest methods.

---
+++ Part1_Front_end_Development/Module7_Testing/UnitTestingBasics/TestingWith_Jest/Exercise1/functions.test.js
@@ -0,0 +1,52 @@
+const functions = require('./functions');
+// console.log('these are functions in functions.js', functions);
+/*
+test('Should be null', () => {
+        // Start with expect()
+        // Inside the first brackets put what you want to test. That will be a function.
+        // Then follows your .toBeSomethingSomething function,
+        // find the appropriate function in the documentation.
+        // In this case we want to check if the function "isNull()" actually
+        // returned null. So "toBeNull()" makes the most sense
+  expect(functions.isNull()).toBeNull();
+});
+
+test('checkValue Should be falsy when argument is undefined', () => {
+    // expect(functions.checkValue('someshit')).toBeFalsy();
+    expect(functions.checkValue()).toBeFalsy();
+});
+
+
+test('User should be Brad Traversy object', () => {
+    console.log('return value is: ', functions.createUser())
+    expect(functions.createUser()).toEqual({ firstName: 'Brad', lastName: 'Traversy' });
+});
+
+
+*/
+
+
+
+
+/*
+// Tests are really just functions. To demonstrate that, 
+// we now let you create tests that work without outside input:
+
+// Less than or greater than
+test('Should be under or equal to 1600', () => {
+        const load1 = 800;
+        const load2 = 800;
+        // expect(load1 + load2).toBe.....
+});
+
+// Regex
+test('There is no I in team', () => {
+        //  expect('team').not.to..........;
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+     usernames = ['john', 'karen', 'admin'];
+     // expect(usernames).to......
+});
+*/
