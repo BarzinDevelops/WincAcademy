170087f4aeac9b2b2cd839557dfb4d96240f07ed
Barzin <barzin.development@gmail.com>
2022-09-25 13:27:27 +0200

    Tested all jest methods in Exercise1 (of UnitTestingBasics submodule). Need to check WINC's solutions for this exercise.

--- Part1_Front_end_Development/Module7_Testing/UnitTestingBasics/TestingWith_Jest/Exercise1/functions.test.js
+++ Part1_Front_end_Development/Module7_Testing/UnitTestingBasics/TestingWith_Jest/Exercise1/functions.test.js
@@ -1,6 +1,6 @@
 const functions = require('./functions');
 // console.log('these are functions in functions.js', functions);
-/*
+/*  */
 test('Should be null', () => {
         // Start with expect()
         // Inside the first brackets put what you want to test. That will be a function.
@@ -22,13 +22,6 @@ test('User should be Brad Traversy object', () => {
     expect(functions.createUser()).toEqual({ firstName: 'Brad', lastName: 'Traversy' });
 });
 
-
-*/
-
-
-
-
-/*
 // Tests are really just functions. To demonstrate that, 
 // we now let you create tests that work without outside input:
 
@@ -36,17 +29,27 @@ test('User should be Brad Traversy object', () => {
 test('Should be under or equal to 1600', () => {
         const load1 = 800;
         const load2 = 800;
-        // expect(load1 + load2).toBe.....
+        expect(load1 + load2).toBeLessThanOrEqual(1600);
 });
 
 // Regex
 test('There is no I in team', () => {
-        //  expect('team').not.to..........;
+    expect('team').not.toMatch(/I/);         
+    // console.log("'team'.search(/I/) =>",'team'.search(/I/));
+    // console.log('team'.search(/m/) > -1 ? 
+    //                 ("team.search(/m/) => "+'team'.search(/m/)) : 
+    //                 "no shit found");
 });
+
+
+
 
 // Arrays
 test('Admin should be in usernames', () => {
      usernames = ['john', 'karen', 'admin'];
-     // expect(usernames).to......
+     usernames.forEach((item, i) =>console.log(item === 'admin' ? 
+                                                `Found ${item} at position: ${i}`:
+                                                `No match at position$ {i}`));
+     expect(usernames).toContain('admin');
+     
 });
-*/
