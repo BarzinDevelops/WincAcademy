207d50d8502277d05aa3e94bf55284519f6cbe02
Barzin <barzin.development@gmail.com>
2022-10-01 15:49:45 +0200

    create functions that correspond to the tests (for mendatory requirements).

--- Part1_Front_end_Development/Module7_Testing/UnitTestingAdvanced/Exercise1_PasswordVerifier/psw_verifier.js
+++ Part1_Front_end_Development/Module7_Testing/UnitTestingAdvanced/Exercise1_PasswordVerifier/psw_verifier.js
@@ -15,13 +15,29 @@ const conditionsResults = password => [
                             hasDigit(password)
                         ];
 
+// create function that checks if all mendatory requirements are met:
+const minimumRequired = {
 
+    /* Minimum requirements:
+        1. At least 3 of the above conditions are true
+        2. Condition 4 (=Password has 1 or more lowercase characters) is true 
+        (so that must always be true)
+    */
+    // count condition results that were met:
+    passedConditionsCount: password => conditionsResults(password).filter(condition => condition === true).length > 2,
+    
+    /* If requirement both (requirement 1 and 2) are met, 
+        return true and false otherwise:  */
+    minimunRequirementsPassed: password => (this.passedConditionsCount && conditionsResults(password)[3]) ? true : false
+
+}
 
 const verifyPassword = (password) =>{
     
 }
 module.exports = {
-    conditionsResults,
+    // conditionsResults,
+    minimumRequired,
     
 };
 
